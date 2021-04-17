e819eabbb45789399447b57d4c2ffde6dd3bcbee
icy <i@icys.top>
2021-04-18 00:21:34 +0800

    Site updated: 2021-04-18 00:21:32

--- sw.js
+++ sw.js
@@ -26,15 +26,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/146781432.html');
 
-  precacheUrls.push('/posts/936657584.html');
+  precacheUrls.push('/posts/2754913500.html');
 
-  precacheUrls.push('/posts/3116496963.html');
+  precacheUrls.push('/posts/936657584.html');
 
   precacheUrls.push('/posts/860359347.html');
 
-  precacheUrls.push('/posts/2754913500.html');
-
   precacheUrls.push('/posts/3450077458.html');
+
+  precacheUrls.push('/posts/3116496963.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
