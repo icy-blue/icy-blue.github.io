d0837f71937fe8d5c03ee312f40379a2997160d2
icy <i@icys.top>
2021-04-18 00:07:52 +0800

    Site updated: 2021-04-18 00:07:50

--- sw.js
+++ sw.js
@@ -28,11 +28,11 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/936657584.html');
 
+  precacheUrls.push('/posts/3116496963.html');
+
   precacheUrls.push('/posts/860359347.html');
 
   precacheUrls.push('/posts/2754913500.html');
-
-  precacheUrls.push('/posts/3116496963.html');
 
   precacheUrls.push('/posts/3450077458.html');
 
