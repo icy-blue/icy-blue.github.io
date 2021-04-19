ffd14d4a3bfba3cf83911468629ecc40cb8e0914
icy <i@icys.top>
2021-04-20 00:18:13 +0800

    Site updated: 2021-04-20 00:18:11

--- sw.js
+++ sw.js
@@ -32,9 +32,9 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/860359347.html');
 
-  precacheUrls.push('/posts/3450077458.html');
-
   precacheUrls.push('/posts/3116496963.html');
+
+  precacheUrls.push('/posts/3450077458.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
